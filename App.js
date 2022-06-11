3abcdbe47d463534a5295a06ed41ff4a8d380fa8
HDE <houdier@163.com>
2022-06-11 13:40:01 +0800

    commit

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { FlatList, StyleSheet, Text, View, SafeAreaView, Button } from "react-native";
+import { NavigationContainer, getFocusedRouteNameFromRoute } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/FontAwesome'
+import Explore from "./src/containers/Explore/Explore"
+import HistoryScreen from "./src/containers/HistoryScreen";
+import SettingScreen from "./src/containers/SettingScreen";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const Tab = createBottomTabNavigator();
+
+
+class App extends React.Component {
+  constructor() {
+    super();
+  }
+
+
+  render() {
+    return (
+        <NavigationContainer>
+          <Tab.Navigator initialRouteName='Explore'>
+            <Tab.Screen name="Explore"
+                        component={Explore}
+                        options={{
+                            headerShown: false,
+                            tabBarIcon: ({size, color}) => (<Icon name={"plane"} color={color} size={size}/>)
+                        }}
+            />
+            <Tab.Screen name="History"
+                        component={HistoryScreen}
+                        options={{
+                            tabBarIcon: ({size, color}) => (<Icon name={"history"} color={color} size={size}/>)
+                        }} />
+            <Tab.Screen name="Settings" component={SettingScreen}
+                        options={{
+                tabBarIcon: ({size, color}) => (<Icon name={"cog"} color={color} size={size}/>)
+            }} />
+          </Tab.Navigator>
+        </NavigationContainer>
+    );
+  };
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 100
   },
 });
+
+export default App
